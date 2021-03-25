aa2bcd2c227f65b2dd47a27f36d096a3cfa0c7d9
Dom Udomsak <udomsak.mak@ngernturbo.com>
2021-03-25 14:19:45 +0700

    color marker

--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -1,5 +1,16 @@
 import React from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import { divIcon } from 'leaflet'
+
+const icons = {
+    xxSmall: divIcon({className: 'marker-icon pink', iconSize: [12, 12]}),
+    xSmall: divIcon({className: 'marker-icon pink', iconSize: [16, 16]}),
+    small: divIcon({className: 'marker-icon pink', iconSize: [24, 24]}),
+    normal: divIcon({className: 'marker-icon purple', iconSize: [32, 32]}),
+    large: divIcon({className: 'marker-icon purple', iconSize: [48, 48]}),
+    xLarge: divIcon({className: 'marker-icon red', iconSize: [72, 72]}),
+    xxLarge: divIcon({className: 'marker-icon red', iconSize: [96, 96]})
+}
 
 function MapView(props) {
     const { locationArray } = props
@@ -7,8 +18,29 @@ function MapView(props) {
         const {
             id, country_code,
             country, province,
-            coordinates: { latitude, longitude }
+            coordinates: { latitude, longitude },
+            latest: { confirmed }
         } = location
+
+        let markerIcon = icons.xxSmall
+        if(confirmed >= 101 && confirmed <=500) {
+            markerIcon = icons.xSmall
+        }
+        else if(confirmed >= 501 && confirmed <=1000) {
+            markerIcon = icons.small
+        }
+        else if(confirmed >= 1001 && confirmed <=5000) {
+            markerIcon = icons.normal
+        }
+        else if(confirmed >= 5001 && confirmed <=10000) {
+            markerIcon = icons.large
+        }
+        else if(confirmed >= 10001 && confirmed <=50000) {
+            markerIcon = icons.xLarge
+        }
+        else if(confirmed >= 50001) {
+            markerIcon = icons.xxLarge
+        }
 
         let title = country
         if (province !== '' && province !== country) {
@@ -16,7 +48,10 @@ function MapView(props) {
         }
 
         return (
-            <Marker key={`${id}-${country_code}`} position={[latitude, longitude]}>
+            <Marker 
+                key={`${id}-${country_code}`} 
+                position={[latitude, longitude]}
+                icon={markerIcon}>
                 <Popup>{title}</Popup>
             </Marker>
         )

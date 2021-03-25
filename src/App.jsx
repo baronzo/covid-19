b31f0a107b556f61c6bc68cc3ebf796c1abd18c6
Dom Udomsak <udomsak.mak@ngernturbo.com>
2021-03-25 13:58:45 +0700

    add marker

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import MapView from './Components/MapView'
 import 'leaflet/dist/leaflet.css'
 import './Css/App.scss'
+import Axios from 'axios'
+
+const api = 'https://coronavirus-tracker-api.herokuapp.com/v2/locations'
 
 function App() {
+  const [locationArray, setLocationArray] = useState([])
+  useEffect(() => {
+    Axios.get(api).then(response => {
+      setLocationArray(response.data.locations)
+    }).catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <MapView />
+      <MapView locationArray={locationArray}/>
     </div>
   );
 }
--- src/Components/MapView.jsx
+++ src/Components/MapView.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
-import { MapContainer, TileLayer } from 'react-leaflet'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
-function MapView() {
+function MapView(props) {
+    const { locationArray } = props
+    const markerElements = locationArray.map(location => {
+        const {
+            id, country_code,
+            country, province,
+            coordinates: { latitude, longitude }
+        } = location
+
+        let title = country
+        if (province !== '' && province !== country) {
+            title = `${province}, ${country}`
+        }
+
+        return (
+            <Marker key={`${id}-${country_code}`} position={[latitude, longitude]}>
+                <Popup>{title}</Popup>
+            </Marker>
+        )
+    })
     return(
         <MapContainer className='map-view' center={[13, 100]} zoom={5} scrollWheelZoom={true}>
             <TileLayer
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            {markerElements}
         </MapContainer>
     )
 }
